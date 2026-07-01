f74d97d1dc966fa46f835a448777663b2f4b8850
Site Dev <dev@personal-site.local>
2026-07-01 12:10:00 +0800

    fix(web): align config route style import

--- web/src/routes/ConfigRoute.tsx
+++ web/src/routes/ConfigRoute.tsx
@@ -73,7 +73,7 @@ import {
 import { LazyJsonCodeMirror } from "../components/editor/LazyJsonCodeMirror";
 import { VButton, VNativeInput, VNativeSelect, VNativeTextarea } from "../components/vui";
 import { safeAgentCenterReturnToPath } from "./agentCenterRoutes";
-import styles from "./ConfigRoute.styles";
+import styles from "./ConfigRoute.module.css";
 
 type ConfigLanguage = "zh" | "en";
 type NoticeTone = "neutral" | "success" | "error";
